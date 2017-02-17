7b177577192ce31885b8f1f2a071fb4533ba7d3d
Brian Hayes <bjoeyhayes@gmail.com>
2017-02-17 18:17:42 -0500

    progress at least

--- _src/js/modules/expand.js
+++ _src/js/modules/expand.js
@@ -3,17 +3,30 @@
 function Expand(element, options) {
   this.$element = element;
   this.options = options || {};
+  this.originalPosition = this.getElementPosition();
 }
 
 Expand.prototype = {
   constructor: Expand,
 
+  // Get position of element
+  getElementPosition: function() {
+    return this.$element.offset();
+  },
+  // Get height usable space
+    // Get height of viewport
+    // Get height of topbar
+    // Subtract topbar from viewport to get usable height.
+
   openEvent: function() {
     console.log('open now');
-    this.$element.addClass('active');
+    let position = this.originalPosition;
+    let destination = {top: 0};
+    console.log(position);
+    this.$element.css(this.originalPosition).css(destination).addClass('active');
   },
   closeEvent: function() {
-    this.$element.removeClass('active');
+    this.$element.removeClass('active').css(this.originalPosition);
   },
   bindEvents: function() {
     let $elementToBind = this.$element.children('.titled-callout-title');
