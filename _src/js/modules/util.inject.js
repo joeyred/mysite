508a988cace0c5ff30967cafdf074f1ec43df648
Brian Hayes <bjoeyhayes@gmail.com>
2017-02-21 18:56:00 -0500

    making progress.

--- _src/js/modules/util.inject.js
+++ _src/js/modules/util.inject.js
@@ -5,8 +5,9 @@
 class Inject {
   constructor(parentElement, options = {}) {
     this.$element = parentElement.find(`[${this.options.dataAttr}]`);
-    this.path = this.$element.attr(`[${this.options.dataAttr}]`);
-    this.statusAttr = `[${this.options.dataAttr}-status]`;
+    this.path = this.$element.attr(`${this.options.dataAttr}`);
+    this.statusAttr = `${this.options.dataAttr}-status`;
+    this.xhr = new XMLHttpRequest();
     this.options = $.extend(true, this.defaults, options);
     this.defaults = {
       dataAttr:       'data-inject',
@@ -17,5 +18,21 @@ class Inject {
       }
     };
   }
+  contentHasLoaded() {
+    if (this.currentStatusAttrValue() === this.options.dataAttrStatus.loaded) {
+      return true;
+    }
+    return false;
+  }
+  contentIsReady() {
+    return this.xhr.readyState === 4;
+  }
+  currentStatusAttrValue() {
+    return this.$element.attr(this.statusAttr);
+  }
+  updateStatusAttr(status) {
+    this.$element.attr(this.statusAttr, status);
+  }
+
 }
 }(jQuery);
