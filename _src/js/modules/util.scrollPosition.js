feae2d70fe37c20afd32a8df308355b5e3954f76
Brian Hayes <bjoeyhayes@gmail.com>
2017-03-02 14:18:46 -0500

    need a scroll position utility, so one was written.

---
+++ _src/js/modules/util.scrollPosition.js
@@ -0,0 +1,37 @@
+'use strict';
+
+!function($) {
+
+class ScrollPosition {
+  constructor(element) {
+    this.$element = element;
+    this.$window = $(window);
+    this.position = 0;
+    this.lastPosition = 0;
+    this.bindEvents();
+  }
+  updatePosition(event) {
+    this.position = this.getPosition(event);
+    // this.injectPositionValue();
+    // console.log(this.position, 'scrolled');
+  }
+  getPosition() {
+    return this.$window.scrollTop();
+  }
+  bindEvents() {
+    this.$window.scroll(() => this.updatePosition());
+  }
+  // injectPositionValue() {
+  //   this.$element.attr('data-scroll', this.position);
+  // }
+  setLastPosition() {
+    this.lastPosition = this.position;
+  }
+  restoreLastPosition() {
+    this.$element.scrollTop(this.lastPosition);
+  }
+}
+
+Gingabulous.ScrollPosition = ScrollPosition;
+
+}(jQuery);
