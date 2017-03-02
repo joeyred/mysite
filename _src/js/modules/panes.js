7e59bd42aff527fa72efec8f53b544bc62ef6d44
Brian Hayes <bjoeyhayes@gmail.com>
2017-03-02 14:19:55 -0500

    tentative freeze/unfreeze functionality for main pane

--- _src/js/modules/panes.js
+++ _src/js/modules/panes.js
@@ -1,3 +1,5 @@
+'use strict';
+
 !function($) {
 
 // IDEA The Basic Concept:
@@ -25,8 +27,9 @@ class Panes {
 
   get defaults() {
     return {
-      dataAttr:     Gingabulous.modules.Panes.dataAttr,
-      paneDataAttr: 'data-pane'
+      dataAttr:           Gingabulous.modules.Panes.dataAttr,
+      paneDataAttr:       'data-pane',
+      translateXDistance: '100%';
     };
   }
   get attr() {
@@ -59,11 +62,21 @@ class Panes {
   get panesTarget() {
     return `[${this.options.dataAttr}]`;
   }
-
+  /**
+   * Creates a new insatnce of Panels
+   * @class
+   * @requires ScrollPosition
+   * @requires Debug
+   *
+   * @param {jQuery} element - jQuery object that is the highest panes ansestor.
+   * @param {Object} options - Can Override default ui module config.
+   */
   constructor(element, options = {}) {
     this.$element = element;
+    this.$window = $(window);
     this.options = $.extend(true, this.defaults, options);
     this.debug = new Debug('Panes', true);
+    this.scroll = new Gingabulous.ScrollPosition($(this.target.main));
     this.state = {
       active:   'main',
       previous: false
@@ -74,6 +87,28 @@ class Panes {
     this.registerMainPane();
     $(this.paneTarget).each((index, element) => this.registerPane(index, element));
     this.debug.values('init', {panes: this.panes});
+    this.bindEventsToEachOpen();
+    this.bindEventsToEachClose();
+  }
+  updateState(id) {
+    if (this.state.active !== id) {
+      this.state.previous = this.state.active;
+      this.state.active = id;
+      this.updatePositions();
+    }
+  }
+  updatePositions() {
+    let pane = this.panes[this.state.active];
+    this.debug.values('updatePositions', {pane, main: this.panes.main});
+
+    if (!Array.isArray(pane) && this.state.active !== 'main') {
+      pane.css({transform: 'translateX(0)'});
+      this.panes.main.css({transform: 'translateX(100%)'});
+    }
+    if (this.state.active === 'main') {
+      pane.removeAttr('style');
+      this.panes[this.state.previous].css({transform: 'translateX(-100%)'});
+    }
   }
   registerMainPane() {
     this.panes.main = $(this.target.main);
@@ -86,32 +121,104 @@ class Panes {
       let children = false;
       let hasChildren = $pane.children(this.target.parent).attr(this.attr.parent);
       // does this pane have children?
-      if (hasChildren !== undefined) {
+      if (hasChildren === undefined) {
+        this.panes[key] = $pane;
+      } else {
         children = [];
         // time to count the children.
         $pane.children(this.target.parent).children(this.target.pane).each(function() {
           // shove the child into the happy funtime playpen array.
           children.push($(this));
         });
+        this.panes[key] = [$pane, children];
       }
-      this.panes[key] = [$pane, children];
 
       this.debug.values('registerPane', {$pane, key, hasChildren, children});
     }
   }
-  // bindOpenEvents() {
-  //
-  // }
-  // bindEvents() {}
-  // paneEvents() {}
-  // updateActivePane(id) {
-  //   this.activePane = id;
-  // }
-  //
-  // openPane() {
-  //
-  // }
-  // closePane() {}
+  translate(value, direction = 'x') {
+    if (direction === 'x') {
+      return `translateX(${value})`;
+    }
+    if (direction === 'y') {
+      return `translateY(${value})`;
+    }
+    if (direction === 'z') {
+      return `translateZ(${value})`;
+    }
+  }
+  // ///////////////// //
+  // Main Pane Methods //
+  // ///////////////// //
+  makeMainPaneInactive() {
+    // Save the scroll position to restore it later.
+    this.scroll.setLastPosition();
+
+    // set fixed height & width and make overflow hidden.
+    // QUESTION Do we need the 'pointer-events' property set to `none`?
+    this.freezeMainPane();
+  }
+  unfreezeMainPane() {
+    this.panel.main.css(freezeScrollStyles());
+    // Restore scroll position before pane was made inactive.
+    this.scroll.restoreLastPosition();
+  }
+  freezeMainPane() {
+    let height = this.$window.height();
+    let width = this.$window.width();
+    this.panel.main.css({freezeScrollStyles(height, width, 'hidden'));
+  }
+  freezeScrollStyles(height = '', width = '', overflow = '') {
+    return {height, width, overflow};
+  }
+
+
+
+  // /////////////////// //
+  // Common Pane Methods //
+  // /////////////////// //
+
+  // //////////////////// //
+  // Open Trigger Methods //
+  // //////////////////// //
+
+  // ///////////////////// //
+  // Close Trigger Methods //
+  // ///////////////////// //
+
+  // /////////////////// //
+  // Parent Pane Methods //
+  // /////////////////// //
+
+  bindEventsToEachOpen() {
+    $(this.target.open).each((index, element) => this.bindOpenEvents(index, element));
+  }
+  bindEventsToEachClose() {
+    $(this.target.close).each((index, element) => this.bindCloseEvents(index, element));
+  }
+  openEvents(id) {
+    this.updateState(id);
+    console.log(this.state);
+  }
+  closeEvents() {
+    this.updateState('main');
+    console.log(this.state);
+  }
+  bindOpenEvents(index, element) {
+    let $element = $(element);
+    let id = $element.attr(this.attr.open);
+
+    this.debug.values('bindOpenEvents', {$element, id});
+    $element.click(() => this.openEvents(id));
+  }
+  bindCloseEvents(index, element) {
+    let $element = $(element);
+    let id = $element.attr(this.attr.open);
+
+    this.debug.values('bindOpenEvents', {$element, id});
+    $element.click(() => this.closeEvents(id));
+  }
+
 }
 
 Gingabulous.registerModule(Panes);
