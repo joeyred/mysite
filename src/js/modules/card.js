e772c4c82bb5071fd29e2028060913d7cb6fb285
Brian Hayes <bjoeyhayes@gmail.com>
2017-06-02 15:27:48 -0400

    expand functionality is working.

--- src/js/modules/card.js
+++ src/js/modules/card.js
@@ -8,7 +8,7 @@ class Card {
   get defaults() {
     return {
       dataAttr: Gingabulous.modules.Card.dataAttr,
-      classes: {
+      classes:  {
         expand: 'expanded'
       }
     };
@@ -22,15 +22,41 @@ class Card {
       close: `${this.options.dataAttr}-close`
     };
   }
+  init() {
+    console.log('fired init');
+    this._events();
+    this._injectBorderLeft();
+    this._setBorderLeft();
+  }
+  _elementHeight() {
+    return this.element.offsetHeight;
+  }
+  _injectBorderLeft() {
+    let html = `<div class="border-left"></div>`;
+    this.element.insertAdjacentHTML('afterbegin', html);
+  }
+  _setBorderLeft(reducedHeight = false) {
+    let borderElement = this.element.querySelectorAll('.border-left');
+    console.log(borderElement[0]);
+    console.log(this._elementHeight());
+    let elementHeight = this.elementHeight();
+    if (reducedHeight) {
+      borderElement.style.height = `${elementHeight - 10}px`;
+    }
+    if (!reducedHeight) {
+      borderElement.style.height = `${elementHeight}px`;
+    }
+  }
   _expand() {
     this.element.classList.add(this.classes.expand);
   }
   _collapse() {
     this.element.classList.remove(this.classes.expand);
   }
-  _event() {
+  _events() {
     this.element.addEventListener('click', (event) => {
       if (!this.element.classList.contains(this.classes.expand)) {
+        console.log(`I've been clicked to open`);
         this._expand();
       }
       if (this.element.classList.contains(this.classes.expand)) {
