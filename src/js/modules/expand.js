0af76c7751270dfa7f13841c11ec2243f688d875
Brian Hayes <bjoeyhayes@gmail.com>
2017-12-21 17:13:26 -0500

    add responsive behavior and state to module

--- src/js/modules/expand.js
+++ src/js/modules/expand.js
@@ -3,15 +3,17 @@
 class Expand {
   constructor(element, options) {
     this.element = element;
-    this.options = Gingabulous.deepExtend({}, this.defaults, options);
+    this.options = Gingabulous.deepExtend({}, Expand.defaults(), options);
+    this.responsive = new Gingabulous.Responsive(element);
   }
-  get defaults() {
+  static defaults() {
     return {
-      dataAttr:     Gingabulous.modules.Expand.dataAttr,
-      class:        'expandable',
-      type:         'basic',
-      defaultState: 'collapsed',
-      states:       {
+      dataAttr:      Gingabulous.modules.Expand.dataAttr,
+      class:         'expandable',
+      type:          'basic',
+      defaultState:  'collapsed',
+      inactiveState: 'inactive',
+      states:        {
         expanded:  'expanded',
         collapsed: 'collapsed'
       }
@@ -31,23 +33,48 @@ class Expand {
     };
   }
   init() {
+    // console.log('active on init:', this.responsive.isActive());
     // Set Default State
-    this.element.setAttribute(this.attr.expand, this.options.defaultState);
+    if (this.responsive.isActive()) {
+      this.element.setAttribute(this.attr.expand, this.options.defaultState);
+    }
     // Handle Events
     this._events();
   }
   _setState(state) {
     this.element.setAttribute(this.attr.expand, state);
   }
-  _events() {
-    this.element.addEventListener('click', (event) => {
-      // Open Event
-      if (event.target.hasAttribute(this.attr.open)) {
-        this._setState(this.options.states.expanded);
+  _onResize() {
+    if (this.responsive.isActive()) {
+      // If current state doesnt match default state, then set state to default state.
+      if (this.element.getAttribute(this.attr.expand) !== this.options.defaultState) {
+        this._setState(this.options.defaultState);
       }
-      // Close Event
-      if (event.target.hasAttribute(this.attr.close)) {
-        this._setState(this.options.states.collapsed);
+    }
+    if (!this.responsive.isActive()) {
+      if (this.element.getAttribute(this.attr.expand) !== this.options.inactiveState) {
+        this._setState(this.options.inactiveState);
+      }
+    }
+  }
+  _events() {
+    // Resize Events
+    if (!Gingabulous.events.resize) {
+      Gingabulous.registerGlobalEventListener('resize', window);
+    }
+    Gingabulous.events.resize.registerCallback(() => this._onResize());
+
+    // Click Events
+    this.element.addEventListener('click', (event) => {
+      if (this.responsive.isActive()) {
+        // Open Event
+        if (event.target.hasAttribute(this.attr.open)) {
+          this._setState(this.options.states.expanded);
+        }
+        // Close Event
+        if (event.target.hasAttribute(this.attr.close)) {
+          this._setState(this.options.states.collapsed);
+        }
       }
     });
   }
